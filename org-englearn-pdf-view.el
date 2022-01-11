956775a1ed7afad1a94107c16431c0f2dc7a07b1
coco24 <1281299809@qq.com>
2022-01-11 21:47:53 +0800

    Allow disabling org-pdftools-link during capture

--- org-englearn-pdf-view.el
+++ org-englearn-pdf-view.el
@@ -1,15 +1,19 @@
 (require 'org-englearn)
 (require 'pdf-view)
 
-(defvar org-englearn-pdf-view-capture-annot t
-  "If nil set, org-englearn-capture will deactivatethe region before org-capture is called, avoiding org-pdftools creating a new annotation on region.")
+(defvar org-englearn-pdf-view-disable-org-pdftools-link nil)
 
 (defun org-englearn-capture-pdf-view ()
   (interactive)
-  (pcase-let ((`(,cap) (pdf-view-active-region-text)))
-    (unless org-englearn-pdf-view-capture-annot
-      (pdf-view-deactivate-region))
-    (org-capture-string (replace-regexp-in-string "\n" " " cap) "e")))
+  (pcase-let ((`(,cap) (pdf-view-active-region-text))
+              (org-pdftools-link-store))
+    (when org-englearn-pdf-view-disable-org-pdftools-link
+      (require 'org-pdftools)
+      (setq org-pdftools-link-store (org-link-get-parameter org-pdftools-prefix :store))
+      (org-link-set-parameters org-pdftools-prefix :store nil))
+    (org-capture-string (replace-regexp-in-string "\n" " " cap) "e")
+    (when (and org-englearn-pdf-view-disable-org-pdftools-link org-pdftools-link-store)
+      (org-link-set-parameters org-pdftools-prefix :store org-pdftools-link-store))))
 
 (provide 'org-englearn-pdf-view)
 ;;; org-englearn-pdf-view.el ends here
